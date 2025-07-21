63d57dfa0193f9c6afbfe02590d73a0841bf901d
liruilong940607 <ruilongli94@gmail.com>
2025-07-20 19:59:13 -0700

    cleanup mha.py and transformer.py

--- prope/utils/mha.py
+++ prope/utils/mha.py
@@ -1,10 +1,11 @@
-from typing import Callable, Optional, Tuple
+from typing import Callable, Optional
 
 import torch
 import torch.nn.functional as F
 from einops import rearrange
 from torch import Tensor
-from torch.nn import MultiheadAttention
+from torch.nn.parameter import Parameter
+from torch.nn.init import constant_, xavier_uniform_
 
 
 # src: https://github.com/pytorch/benchmark/blob/main/torchbenchmark/models/llama/model.py#L28
@@ -23,102 +24,103 @@ class RMSNorm(torch.nn.Module):
         return output * self.weight.type_as(x)
 
 
-class MultiheadAttention(torch.nn.MultiheadAttention):
-    """Wrapper around torch.nn.MultiheadAttention to support customized scaled dot-product attention.
-
-    The __init__ method accepts an additional argument `sdpa_fn` which defines how the
-    scaled dot-product attention is computed. If `sdpa_fn` is not provided, the default
-    behavior of torch.nn.MultiheadAttention is used (i.e., F.scaled_dot_product_attention).
-
-    The `sdpa_fn` function should have the following signature:
-    ```
-    def sdpa_fn(q, k, v, **kwargs) -> Tensor:
-    ```
+class MultiheadAttention(torch.nn.Module):
+    """
+    Same as torch.nn.MultiheadAttention for bidirectional attention, but supports:
+    - RMSNorm
+    - customized attention function
     """
 
-    def __init__(self, *args, **kwargs):
-        self.qk_norm = kwargs.pop("qk_norm", False)
-        super().__init__(*args, **kwargs)
-        if self.qk_norm:
-            d_model, nhead = args[0], args[1]
-            self.q_norm = RMSNorm(d_model // nhead)
-            self.k_norm = RMSNorm(d_model // nhead)
+    def __init__(
+        self,
+        embed_dim: int,
+        num_heads: int,
+        dropout: float = 0.0,
+        bias: bool = True,
+        qk_norm: bool = False,
+    ):
+        super().__init__()
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.head_dim = embed_dim // num_heads
+        self.dropout = dropout
+        self.bias = bias
+        self.qk_norm = qk_norm
+
+        self.in_proj_weight = Parameter(torch.empty((3 * embed_dim, embed_dim)))
+
+        if self.bias:
+            self.in_proj_bias = Parameter(torch.empty(3 * embed_dim))
+        else:
+            self.register_parameter("in_proj_bias", None)
+
+        self.out_proj = torch.nn.Linear(embed_dim, embed_dim, bias=bias)
+
+        if qk_norm:
+            self.q_norm = RMSNorm(self.head_dim)
+            self.k_norm = RMSNorm(self.head_dim)
+        else:
+            self.q_norm = None
+            self.k_norm = None
+
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        xavier_uniform_(self.in_proj_weight)
+        if self.in_proj_bias is not None:
+            constant_(self.in_proj_bias, 0.0)
+            constant_(self.out_proj.bias, 0.0)
 
     def forward(
         self,
         query: Tensor,
         key: Tensor,
         value: Tensor,
-        key_padding_mask: Optional[Tensor] = None,
-        need_weights: bool = True,
-        attn_mask: Optional[Tensor] = None,
-        average_attn_weights: bool = True,
-        is_causal: bool = False,
-        sdpa_fn: Optional[Callable] = None,
-    ) -> Tuple[Tensor, Optional[Tensor]]:
-        if sdpa_fn is None:
-            return super().forward(
-                query,
-                key,
-                value,
-                key_padding_mask=key_padding_mask,
-                need_weights=need_weights,
-                attn_mask=attn_mask,
-                average_attn_weights=average_attn_weights,
-                is_causal=is_causal,
-            )
+        sdpa_fn: Callable = F.scaled_dot_product_attention,
+    ) -> Tensor:
+        """
+        Args:
+            query: (B, T, D)
+            key: (B, T, D)
+            value: (B, T, D)
+            sdpa_fn: (q, k, v, **kwargs) -> Tensor
+        Returns:
+            output: (B, T, D)
+        """
+        q_proj_weight, k_proj_weight, v_proj_weight = self.in_proj_weight.chunk(
+            3, dim=0
+        )
+        if self.in_proj_bias is not None:
+            q_proj_bias, k_proj_bias, v_proj_bias = self.in_proj_bias.chunk(3, dim=0)
         else:
-            # manually compute mha with customized sdpa_fn
-            assert self.bias_k is None and self.bias_v is None
-            assert not self.add_zero_attn
-            assert not need_weights
+            q_proj_bias = k_proj_bias = v_proj_bias = None
 
-            if not self.batch_first:
-                query, key, value = (x.permute(1, 0, 2) for x in (query, key, value))
+        q = F.linear(query, q_proj_weight, q_proj_bias)
+        k = F.linear(key, k_proj_weight, k_proj_bias)
+        v = F.linear(value, v_proj_weight, v_proj_bias)
+        q, k, v = (
+            rearrange(x, "b t (h c) -> b t h c", h=self.num_heads) for x in [q, k, v]
+        )
+        if self.qk_norm:
+            q = self.q_norm(q)
+            k = self.k_norm(k)
+        q, k, v = (rearrange(x, "b t h c -> b h t c") for x in [q, k, v])
+        o = sdpa_fn(q, k, v, dropout_p=self.dropout if self.training else 0.0)
+        o = rearrange(o, "b h t c -> b t (h c)", h=self.num_heads)
+        attn_output = F.linear(o, self.out_proj.weight, self.out_proj.bias)
 
-            if self._qkv_same_embed_dim:
-                q_proj_weight, k_proj_weight, v_proj_weight = self.in_proj_weight.chunk(
-                    3, dim=0
-                )
-            else:
-                q_proj_weight = self.q_proj_weight
-                k_proj_weight = self.k_proj_weight
-                v_proj_weight = self.v_proj_weight
-
-            if self.in_proj_bias is not None:
-                q_proj_bias, k_proj_bias, v_proj_bias = self.in_proj_bias.chunk(
-                    3, dim=0
-                )
-            else:
-                q_proj_bias = k_proj_bias = v_proj_bias = None
-
-            q = F.linear(query, q_proj_weight, q_proj_bias)
-            k = F.linear(key, k_proj_weight, k_proj_bias)
-            v = F.linear(value, v_proj_weight, v_proj_bias)
-            q, k, v = (
-                rearrange(x, "b t (h c) -> b t h c", h=self.num_heads)
-                for x in [q, k, v]
-            )
-            if self.qk_norm:
-                q = self.q_norm(q)
-                k = self.k_norm(k)
-            q, k, v = (rearrange(x, "b t h c -> b h t c") for x in [q, k, v])
-            o = sdpa_fn(q, k, v, dropout_p=self.dropout)
-            o = rearrange(o, "b h t c -> b t (h c)", h=self.num_heads)
-            attn_output = F.linear(o, self.out_proj.weight, self.out_proj.bias)
-
-            return attn_output, None
+        return attn_output
 
 
 if __name__ == "__main__":
     torch.manual_seed(42)
 
     device = "cuda:0"
-    mha = MultiheadAttention(embed_dim=128, num_heads=8).to(device)
+    mha = MultiheadAttention(embed_dim=32, num_heads=8).to(device)
 
-    q = torch.randn(10, 32, 128).to(device)
-    k = torch.randn(20, 32, 128).to(device)
-    v = torch.randn(20, 32, 128).to(device)
+    q = torch.randn(10, 16, 32).to(device)
+    k = torch.randn(20, 16, 32).to(device)
+    v = torch.randn(20, 16, 32).to(device)
 
-    output, _ = mha(q, k, v, sdpa_fn=lambda q, k, v, **kwargs: v, need_weights=False)
+    output = mha(q, k, v, sdpa_fn=lambda q, k, v, **kwargs: v)
     print(output.size(), output.sum())
